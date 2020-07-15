4b30b4f872d3fe23306a9c31d1212f191fb9d5f7
Chris Brooks <chris2brooks@gmail.com>
2020-07-15 18:36:38 +0200

    Setup Prismic connection

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,7 +54,7 @@ const Footer = () => (
             <Logo />
         </Link>
         <FooterAuthor href="https://www.marguerite.io/?utm_source=prist&utm_medium=footer&utm_campaign=prist_starter">
-            © 2019 — Designed & developed by Marguerite Roth
+            © 2020 — Designed & developed by Marguerite Roth
             <FooterSpooch className="FooterSpooch" src={spooch} />
         </FooterAuthor>
     </FooterContainer>
